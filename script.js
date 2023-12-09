4c44f916b0f747be5d1f591978cd2d116ff42d2a
Abimanianandan <abikbm06@gmail.com>
2023-12-09 09:05:42 +0530

    remove display function

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const heading=document.createElement("h1");
-const para=document.createElement("p");
+const heading = document.createElement("h1");
+const para = document.createElement("p");
 const division = document.createElement("div");
 const division1 = document.createElement("div");
 const input = document.createElement("input");
@@ -23,133 +23,128 @@ const btn16 = document.createElement("button");
 const btn17 = document.createElement("button");
 const btn18 = document.createElement("button");
 
-
-heading.setAttribute("id","title");
-heading.innerText="Calculator task";
-para.setAttribute("id","description");
-para.innerText="Create a calculator using Dom"
+heading.setAttribute("id", "title");
+heading.innerText = "Calculator task";
+para.setAttribute("id", "description");
+para.innerText = "Create a calculator using Dom";
 division.setAttribute("class", "Container");
 division1.setAttribute("class", "Calculator");
-input.className="outputscreen";
-input.type="text"
-input.placeholder="0";
-input.id="result";
+input.className = "outputscreen";
+input.type = "text";
+input.placeholder = "0";
+input.id = "result";
 
-btn.setAttribute("id","clear")
-btn.setAttribute("onclick", "clear()");
+btn.setAttribute("id", "clear");
 btn.innerText = "C";
 btn.addEventListener("click", () => {
   input.value = "";
 });
 
-btn1.setAttribute("id","delete")
-btn1.setAttribute("onclick", "del()");
+btn1.setAttribute("id", "delete");
+
 btn1.innerText = "<--";
 btn1.addEventListener("click", () => {
   input.value = input.value.slice(0, -1);
 });
 
-btn2.setAttribute("id",".")
-btn2.setAttribute("onclick", "display(.)");
+btn2.setAttribute("id", ".");
 btn2.innerText = ".";
 btn2.addEventListener("click", () => {
   input.value += ".";
 });
-btn3.setAttribute("id","mul")
-btn3.setAttribute("onclick", "display(*)");
+btn3.setAttribute("id", "mul");
+
 btn3.innerText = "X";
 btn3.addEventListener("click", () => {
   input.value += "*";
 });
-btn4.setAttribute("id","7")
-btn4.setAttribute("onclick", "display(7)");
+btn4.setAttribute("id", "7");
+
 btn4.innerText = "7";
 btn4.addEventListener("click", () => {
   input.value += "7";
 });
-btn5.setAttribute("id","8")
-btn5.setAttribute("onclick", "display(8)");
+btn5.setAttribute("id", "8");
+
 btn5.innerText = "8";
 btn5.addEventListener("click", () => {
   input.value += "8";
 });
-btn6.setAttribute("id","9")
-btn6.setAttribute("onclick", "display(9)");
+btn6.setAttribute("id", "9");
+
 btn6.innerText = "9";
 btn6.addEventListener("click", () => {
   input.value += "9";
 });
-btn7.setAttribute("id","divide")
-btn7.setAttribute("onclick", "display(/)");
+btn7.setAttribute("id", "divide");
+
 btn7.innerText = "/";
 btn7.addEventListener("click", () => {
   input.value += "/";
 });
-btn8.setAttribute("id","4")
-btn8.setAttribute("onclick", "display(4)");
+btn8.setAttribute("id", "4");
+
 btn8.innerText = "4";
 btn8.addEventListener("click", () => {
   input.value += "4";
 });
-btn9.setAttribute("id","5")
-btn9.setAttribute("onclick", "display(5)");
+btn9.setAttribute("id", "5");
+
 btn9.innerText = "5";
 btn9.addEventListener("click", () => {
   input.value += "5";
 });
-btn10.setAttribute("id","6")
-btn10.setAttribute("onclick", "display(6)");
+btn10.setAttribute("id", "6");
+
 btn10.innerText = "6";
 btn10.addEventListener("click", () => {
   input.value += "6";
 });
-btn11.setAttribute("id","sub")
-btn11.setAttribute("id","subtract");
-btn11.setAttribute("onclick", "display(-)");
+btn11.setAttribute("id", "sub");
+btn11.setAttribute("id", "subtract");
+
 btn11.innerText = "-";
-btn11.addEventListener("click",()=>{
+btn11.addEventListener("click", () => {
   input.value += "-";
 });
-btn12.setAttribute("id","1")
-btn12.setAttribute("onclick", "display(1)");
+btn12.setAttribute("id", "1");
+
 btn12.innerText = "1";
 btn12.addEventListener("click", () => {
   input.value += "1";
 });
-btn13.setAttribute("id","2")
-btn13.setAttribute("onclick", "display(2)");
+btn13.setAttribute("id", "2");
+
 btn13.innerText = "2";
 btn13.addEventListener("click", () => {
   input.value += "2";
 });
-btn14.setAttribute("id","3")
-btn14.setAttribute("onclick", "display(3)");
+btn14.setAttribute("id", "3");
+
 btn14.innerText = "3";
 btn14.addEventListener("click", () => {
   input.value += "3";
 });
 
-btn15.setAttribute("id","add");
-btn15.setAttribute("onclick","display(+)");
+btn15.setAttribute("id", "add");
+
 btn15.innerText = "+";
-btn15.addEventListener("click",()=>{
- 
-   input.value += "+";
-}); 
-btn16.setAttribute("id","0");
-btn16.setAttribute("onclick", "display(0)");
+btn15.addEventListener("click", () => {
+  input.value += "+";
+});
+btn16.setAttribute("id", "0");
+
 btn16.innerText = "0";
 btn16.addEventListener("click", () => {
   input.value += "0";
-  
 });
-btn17.setAttribute("id","00");
-btn17.setAttribute("onclick", "display(00)");
+btn17.setAttribute("id", "00");
+
 btn17.innerText = "00";
 btn17.addEventListener("click", () => {
   input.value += "00";
 });
-btn18.setAttribute("onclick", "display(=)");
+
 btn18.setAttribute("id", "equal");
 btn18.innerText = "=";
 btn18.addEventListener("click", () => {
@@ -159,12 +154,6 @@ btn18.addEventListener("click", () => {
     alert("Invalid, Only numbers are applicable");
   }
 });
- 
-
-
-
-
-
 
 division1.append(
   input,
